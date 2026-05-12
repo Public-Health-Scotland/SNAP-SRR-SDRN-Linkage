5f3f0a81be6679216c3f5f4e3c9c35df74db32aa
AlexBlanchard-phs <alex.blanchard@phs.scot>
2026-05-12 12:48:18 +0000

    added SIMD data

--- SNAP-SRR-SDRN-Linkage.R
+++ SNAP-SRR-SDRN-Linkage.R
@@ -26,20 +26,25 @@ scidc_cohort <- readr::read_csv(external_cohort_folder, col_types = 'cc') %>%
 Deaths_Full <- read.xlsx('//conf/quality/srr/Active/(04) Project Reports/Annual Reports/2026/Extracts/Deaths_Full.xlsx', detectDates = T)
 SRR_Patients <- read.xlsx('//conf/quality/srr/Active/(04) Project Reports/Annual Reports/2026/Extracts/SRR_Patients_Static.xlsx', detectDates = T)
 
+
+SIMD_Scores <- readRDS("/conf/linkage/output/lookups/Unicode/Deprivation/postcode_2026_1_simd2020v2.rds") %>%
+  select(FormattedPostcode = pc8, simd_quintile = simd2020v2_sc_quintile, simd_decile = simd2020v2_sc_decile)
+
 SRR_Data <- left_join(SRR_Patients, Deaths_Full, by = 'chi_formatted')
 
 FullDatabase <- scidc_cohort %>% 
-  inner_join(SRR_Data, by = 'chi_formatted') 
+  inner_join(SRR_Data, by = 'chi_formatted') %>% 
+  left_join(SIMD_Scores, by = 'FormattedPostcode')
 
 OutputData <- FullDatabase %>% 
   filter(StartKRT > '1981-01-01') %>% 
   filter(date1 < '2024-12-31') %>% 
   select(id, chi_formatted, FormattedPostcode, StartKRT, First_Type, First_Unit, PRD_Code, Sex = Gender,
          Type_Transplant, dt_1st_tx_failed, source_transplant = DBD_DCD_LRD_LUD, Date_Of_Death = date_of_death.y, Ethnicity, 
-         date_first_referral, KRTChangeDate = date1, RRT.Modality, Last_Unit)
+         date_first_referral, KRTChangeDate = date1, RRT.Modality, Last_Unit, simd_quintile, simd_decile)
 
 
-#missing: dt_tx_status, simd_2020v2_sc_decile, quintile
+#missing: dt_tx_status
 #technically missing tx_date but those data are included in the DDT Modality change when a tx occurs.
 
 
@@ -72,6 +77,6 @@ writeData(
 saveWorkbook(
   wb = Wb,
   # File path to save out 
-  file = paste0(OutputFolder,'/ThureaduEDRIS-2425-0260_OutputDataV2.xlsx'),
+  file = paste0(OutputFolder,'/ThureaduEDRIS-2425-0260_OutputDataV3.xlsx'),
   overwrite = TRUE
 )
